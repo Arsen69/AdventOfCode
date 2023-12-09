4bbe1bbfed9adcb710d8a490930c43817d0cf988
Arsen69 <antoine.spanier@gmail.com>
2023-12-09 09:39:29 +0100

    2023 - Jour 9

--- 2023/Day9/Day9.py
+++ 2023/Day9/Day9.py
@@ -3,26 +3,45 @@ import time
 from math import lcm
 
 test_data = """
-LLR
-
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+8 10 9 -2 -17 8
+0 1 1 0
 """
 
 with open(r"2023\Day9\input.txt") as f:
     input = f.read()
 
 def parse_input(input_string):
-    return input_string
+    lines = [[int(x) for x in line.split()] for line in input_string.strip().split('\n')]
+    return lines
 
 def part_1(input_string):
     data = parse_input(input_string)
-    return data
+    ic(data)
+    last_vals = []
+    for sequence in data:
+        last_vals.append(get_next_val(sequence))
+    ic(last_vals)
+    return sum(last_vals)
+
+def get_next_val(sequence):
+    if not any(sequence):
+        return 0
+    lower_sequence = []
+    for i in range(len(sequence) - 1):
+        diff = sequence[i + 1] - sequence[i]
+        lower_sequence.append(diff)
+    return sequence[-1] + get_next_val(lower_sequence)
 
 def part_2(input_string):
     data = parse_input(input_string)
-    return data
+    last_vals = []
+    for sequence in data:
+        sequence.reverse()
+        last_vals.append(get_next_val(sequence))
+    return sum(last_vals)
 
 start_time = time.perf_counter()
 result1 = part_1(input)
